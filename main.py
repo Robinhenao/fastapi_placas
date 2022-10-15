ed8fbc184b6e58e5e92168630819e2885f61141e
Robinhenao <65988533+Robinhenao@users.noreply.github.com>
2022-10-14 23:06:37 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,140 @@
+import os
+from fastapi import FastAPI, Form,UploadFile,File,Body,Request
+from fastapi.responses import JSONResponse,HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import cv2
+import pytesseract
+import numpy as np
+import model
+
+
+pytesseract.pytesseract.tesseract_cmd= r'C:\Program Files\Tesseract-OCR\tesseract'
+
+
+app = FastAPI(title='placas',description='convierte placa en texto',version='1.0')
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates=Jinja2Templates(directory="templates")
+
+@app.get('/',response_class=HTMLResponse)
+def load_index(request: Request):
+    return templates.TemplateResponse("index.html",{"request":request})
+
+@app.post('/submitUpload')
+async def post_upload_file(request: Request,flexRadioDefault:bool=Form(...), upload_file:UploadFile=File(...)):
+    try:
+        with open(upload_file.filename,"wb") as buffer:
+            content =await upload_file.read()
+            buffer.write(content)
+            buffer.close()
+            os.rename(upload_file.filename,"imagen.jpg")
+
+            if(flexRadioDefault):
+                preprocesamiento("imagen.jpg")
+                os.remove("imagen.jpg")
+                segmentacion("proces.jpg")
+                print(pytesseract.image_to_string("letras/0 - letras.jpg",config='--psm 10 --oem 3 -c tessedit_char_whitelist=ABCDEFGHIjJKLMNOPQRSTUVWXYZ-1234567890'))
+                print(pytesseract.image_to_string("letras/1 - letras.jpg",config='--psm 10 --oem 3 -c tessedit_char_whitelist=ABCDEFGHIjJKLMNOPQRSTUVWXYZ-1234567890'))
+                print(pytesseract.image_to_string("letras/2 - letras.jpg",config='--psm 10 --oem 3 -c tessedit_char_whitelist=ABCDEFGHIjJKLMNOPQRSTUVWXYZ-1234567890'))
+                print(pytesseract.image_to_string("letras/3 - letras.jpg",config='--psm 10 --oem 3 -c tessedit_char_whitelist=ABCDEFGHIjJKLMNOPQRSTUVWXYZ-1234567890'))
+                print(pytesseract.image_to_string("letras/4 - letras.jpg",config='--psm 10 --oem 3 -c tessedit_char_whitelist=ABCDEFGHIjJKLMNOPQRSTUVWXYZ-1234567890'))
+                print(pytesseract.image_to_string("letras/5 - letras.jpg",config='--psm 10 --oem 3 -c tessedit_char_whitelist=ABCDEFGHIjJKLMNOPQRSTUVWXYZ-1234567890'))
+                text=pytesseract.image_to_string("proces.jpg",config='--psm 10 --oem 3 -c tessedit_char_whitelist=ABCDEFGHIjJKLMNOPQRSTUVWXYZ-1234567890')
+                os.remove("proces.jpg")
+            else:
+                img=model.preprocesamientoModel("imagen.jpg")
+                os.remove("imagen.jpg")
+                text=model.segmentadoModel(img)
+                #text="model"     
+        return templates.TemplateResponse("show_text.html",{"request":request,"text":text})
+    except FileNotFoundError:
+        return templates.TemplateResponse("index.html",{"request":request})
+    
+
+
+
+def preprocesamiento(ruta):
+    imagen=cv2.imread(ruta)
+    rgB=np.matrix(imagen[:,:,0])
+    rGb=np.matrix(imagen[:,:,1])
+    Rgb=np.matrix(imagen[:,:,2])
+    img=cv2.absdiff(rGb,rgB)
+    [fil,col]=img.shape
+    for i in range(0,fil):
+        for j in range(0,col):
+            if img[i,j]<80:
+                img[i,j]=0
+    for i in range(0,fil):
+        for j in range(0,col):
+            if img[i,j]>0:
+                img[i,j]=1
+    se=np.ones((50,50), np.uint8)
+    se2=np.ones((10,10),np.uint8)
+    closing=cv2.morphologyEx(img,cv2.MORPH_CLOSE,se)
+    dilation=cv2.dilate(closing, se2,1)
+    S,contours,hierarchy=cv2.findContours(dilation,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cnt=contours[:]
+    num=len(cnt)
+    box=np.zeros((num,4))
+    for i in range(0,num):
+        box[i,:]=cv2.boundingRect(cnt[i])
+        L=np.zeros((num,4))
+        Max=[0,0]
+        for i in range (0,num):
+            L[i,:]=box[i]
+            if L[i,2]>Max[1]:
+                Max=[i,L[i,2]]
+    BOX=box[Max[0],:]
+    BOX=np.array(BOX,dtype = np.uint32)
+    b=imagen[BOX[1]:BOX[1]+BOX[3],BOX[0]:BOX[0]+BOX[2],:]
+    cv2.imwrite("proces.jpg",b)
+
+
+
+def segmentacion(rutaImagenRecortada):
+    A=cv2.imread(rutaImagenRecortada)
+    [fil,col,cap]=A.shape
+    rgB=A[:,:,0]
+    rGb=A[:,:,1]
+    Rgb=A[:,:,2]
+    R=Rgb/255.0
+    G=rGb/255.0
+    B=rgB/255.0
+    K=np.zeros((fil,col))
+    for i in range(0,fil):
+        for j in range(0,col):
+            MAX=max(R[i,j],G[i,j],B[i,j])
+            K[i,j]=1-MAX
+    cv2.imwrite("k.bmp",K)
+    k=cv2.imread("k.bmp")
+    BW1=cv2.Laplacian(k,cv2.CV_8UC1)
+    Image=BW1[:,:,0]+BW1[:,:,1]+BW1[:,:,2]
+    ret,thresh=cv2.threshold(Image,0,255,0)
+    S,contours,hierarchy=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cnt=contours[:]
+    num=len(cnt)
+    box=np.zeros((num,4))
+    for j in range (0,num):
+        box[j,:]=cv2.boundingRect(cnt[j])
+    Box=np.zeros((20,4))
+    [L,A]=thresh.shape
+    q=0
+    for j in range(0,num):
+        p=box[j,:]
+        if p[2]>=0.095*A and p[2]<=0.15*A and p[3]>=0.46*L and p[3]<=0.67*L:
+            Box[q]=p
+            q=q+1 
+    Box=np.array(Box,dtype = np.uint32)
+    img=cv2.imread(rutaImagenRecortada)
+
+    scale_percent = 120 
+    width = int(img.shape[1] * 60 / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    for i in range(0,6):
+        let=img[Box[i*2,1]:Box[i*2,1]+Box[i*2,3],Box[i*2,0]:Box[i*2,0]+Box[i*2,2]]
+        resized = cv2.resize(let, dim, interpolation = cv2.INTER_AREA)  
+        cv2.imwrite("letras/"+str(i)+" - letras.jpg",resized)
+    
---
+++ model.py
@@ -0,0 +1,141 @@
+import cv2 
+import pytesseract
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras as  k
+
+
+etiquetas2=np.load('etiquetasD.npy','',allow_pickle=True)
+dictEtiquetas = etiquetas2.tolist()
+
+to_res = (128, 128,3)
+res_model = k.applications.ResNet50(include_top=False, weights="imagenet",input_shape=(128, 128,3))
+model = k.models.Sequential()
+model.add(k.layers.Lambda(lambda image: tf.image.resize(image, to_res))) 
+model.add(res_model)
+model.add(k.layers.Flatten())
+model.add(k.layers.BatchNormalization())
+model.add(k.layers.Dense(256, activation='relu'))
+model.add(k.layers.Dropout(0.5))
+model.add(k.layers.BatchNormalization())
+model.add(k.layers.Dense(128, activation='relu'))
+model.add(k.layers.Dropout(0.5))
+model.add(k.layers.BatchNormalization())
+model.add(k.layers.Dense(64, activation='relu'))
+model.add(k.layers.Dropout(0.5))
+model.add(k.layers.BatchNormalization())
+model.add(k.layers.Dense(37, activation='softmax'))
+
+model = tf.keras.models.load_model('Model_Resnet50_placas.h5')
+model.compile(loss='categorical_crossentropy',optimizer=k.optimizers.RMSprop(lr=2e-5),metrics=['accuracy'])
+
+
+def convertImage(image):
+    s = (128,128)
+    imagenBlanca = np.ones(s, dtype=np.uint8)*255
+    gray2,file1= cv2.threshold(image,125,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    h,w= file1.shape[:2]
+    h_img, w_img = imagenBlanca.shape[:2]
+    center_x = int(w_img/2)
+    center_y = int(h_img/2)
+    top_y = center_y - int(h/2)
+    left_x = center_x - int(w/2)
+    bottom_y = top_y + h
+    right_x = left_x + w
+    roi = imagenBlanca[top_y:bottom_y, left_x:right_x]
+    result = cv2.addWeighted(file1, 1, w, 0, 0)
+    imagenBlanca[top_y:bottom_y, left_x:right_x] = result
+    imagenBlanca = cv2.cvtColor(imagenBlanca, cv2.COLOR_GRAY2RGB)
+    return imagenBlanca
+
+def ocr(imagen):
+    text= ""
+    indicePredicho=0
+    background=[]
+    background = convertImage(imagen)
+    indicePredicho = (model.predict( np.array( [background,] )  )).argmax(axis=1)
+    text = dictEtiquetas[int(indicePredicho)]
+    return text
+
+def preprocesamientoModel(ruta):
+    file1=cv2.imread(ruta)
+    h, s, v = cv2.split(file1)
+    v = cv2.subtract(v, 50)
+    v[v < 0] = 0
+    hsv = cv2.merge((h, s, v))
+    img_np = np.array(hsv)
+    img_np = file1
+    nB = np.matrix(img_np[:,:,0])
+    nG = np.matrix(img_np[:,:,1])
+    nR = np.matrix(img_np[:,:,2])
+    color = cv2.absdiff(nG,nB) #Restar
+    imgSize = np.shape(color)
+    blockSize = int(1 / 8 * imgSize[0] / 2 * 2 + 1)
+    if blockSize <= 1:
+        blockSize = int(imgSize[0] / 2 * 2 + 1)
+    const = 10
+    mask = cv2.adaptiveThreshold(color, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.dilate(mask, kernel, iterations=1)
+    [fil,col] = color.shape
+    for i in range(0,fil):
+        for j in range(0,col):
+            if color[i,j]<80:
+                color[i,j]=0
+
+    for i in range(0,fil):
+        for j in range(0,col):
+            if color[i,j]>0:
+                color[i,j]=1
+
+    color = color *255
+    im2,contornos,hierarchy = cv2.findContours(color,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    candidatos = []
+    for c in contornos:
+        area = cv2.contourArea(c)
+        x,y,w,h = cv2.boundingRect(c)
+        epsilon = 0.09*cv2.arcLength(c,True)
+        approx = cv2.approxPolyDP(c,epsilon,True)   #approximate the shape of polygonal curves
+        if area>759 and area<140000:     # >1000 and <140000:
+            aspect_ratio = float(w)/h
+            if aspect_ratio>1.5 and aspect_ratio<10.3:  #original: 2.07 >1.9
+                placa = file1[y:y+h,x:x+w]
+                candidatos.append(c)
+        
+    return placa
+
+def segmentadoModel(file2):
+    dimensions = (162,78)
+    image = cv2.resize(file2,dimensions)
+    cnts = []
+    gray1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #Actual:
+    gray2 = cv2.blur(gray1,(3,3))
+    canny1 = cv2.Canny(gray2,150,200)
+    canny2 = cv2.dilate(canny1,None,iterations=1)
+    #establecer un umbral
+    umbral = 100
+    ret,umbral_img = cv2.threshold(gray1, umbral, 255, cv2.THRESH_BINARY)
+    im2, cnts, jerarquia = cv2.findContours(umbral_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    i=1
+    lista = list()
+    for i in range(len(cnts)):
+        placa = []
+        area = cv2.contourArea(cnts[i])
+        x,y,w,h = cv2.boundingRect(cnts[i])
+        epsilon = 0.09*cv2.arcLength(cnts[i],True)
+        approx = cv2.approxPolyDP(cnts[i],epsilon,True)   #approximate the shape of polygonal curves
+        if area>300 and area<1000:   #551-4900      #251-4900  #251-1000    #151 - 1000
+            aspect_ratio = float(w)/h
+            if aspect_ratio>0.1 and aspect_ratio<2.6:  #original: 2.4 ; >0.1 <10 <2.6
+                placa = gray2[y:y+h,x:x+w]
+                #text = ""  #No pertenece al código original
+                if placa is None:
+                    continue  
+                text = ocr(placa)
+                lista.append(str(text))
+                print('Caracter Identificado: ',text)
+    text="".join(lista)
+    return text
+
+
